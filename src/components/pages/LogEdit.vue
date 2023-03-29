847840385c4c044b03a5d9119d4dfc6c030c5a42
bri <bcamesi@gmail.com>
2023-03-28 20:18:20 -0400

    edit log styling

--- src/components/pages/LogEdit.vue
+++ src/components/pages/LogEdit.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="main">
-    <h2>Edit Log Entry</h2>
+    <h1>Edit Log Entry for {{ log.date }} at {{ log.time }}</h1>
     <form @submit.prevent="submitForm">
-      <label for="date">Date:</label>
-      <input type="date" id="date" v-model="log.date" /><br/>
-      <label for="time">Time:</label>
-      <input type="time" id="time" v-model="log.time" /><br/>
-      <label for="location">Location:</label>
-      <input type="text" id="location" v-model="log.location" /><br/>
-      <label for="length">Length (Miles):</label>
-      <input id="length-input" type="number" step="0.01" name="length" v-model="log.length" /><br/>
-      <label for="image">Image URL:</label>
-      <input type="text" id="image" v-model="log.image" /><br/>
-      <label for="observations">Observations:</label>
-      <input type="text" id="observations" v-model="log.observations" /><br/>
+      <label for="date">Date:</label><br/>
+      <input type="date" id="date" v-model="log.date" /><br/><br/>
+      <label for="time">Time:</label><br/>
+      <input type="time" id="time" v-model="log.time"/><br/><br/>
+      <label for="location">Location:</label><br/>
+      <input type="text" id="location" v-model="log.location" /><br/><br/>
+      <label for="length">Length (Miles):</label><br/>
+      <input id="length-input" type="number" step="0.01" name="length" v-model="log.length" /><br/><br/>
+      <label for="image">Image URL:</label><br/>
+      <input type="text" id="image" v-model="log.image" /><br/><br/>
+      <label for="observations">Notes/Observations:</label><br/>
+      <textarea type="text" id="observations" v-model="log.observations" /><br/><br/>
       <button type="submit">Save</button>
     </form>
   </div>
