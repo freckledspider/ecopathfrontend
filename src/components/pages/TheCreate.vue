882df8b37004a1146f37a49fbbbfedb544561540
bri <bcamesi@gmail.com>
2023-03-28 20:19:44 -0400

    new log page styling

--- src/components/pages/TheCreate.vue
+++ src/components/pages/TheCreate.vue
@@ -2,18 +2,18 @@
     <div class="main">
       <h1>Create New Hiking Entry</h1>
       <form @submit.prevent="createLog">
-          <label for="date">Date:</label>
-          <input type="date" name="date" v-model="logDate"><br/>
-          <label for="time">Time:</label>
-          <input type="time" name="time" v-model="logTime"><br/>
-          <label for="location">Location:</label>
-          <input type="text" name="location" v-model="logLocation"><br/>
-          <label for="length">Length (Miles):</label>
-          <input id="length-input" type="number" step="0.01" name="length" v-model="logLength"><br/>
-          <label for="image">Image URL:</label>
-          <input type="text" name="image" v-model="logImage"><br/>
-          <label for="observations">Observations:</label>
-          <textarea type="text" name="observations" v-model="logObservations"></textarea><br/>
+          <label for="date">Date:</label><br/>
+          <input type="date" name="date" v-model="logDate"><br/><br/>
+          <label for="time">Time:</label><br/>
+          <input type="time" name="time" v-model="logTime"><br/><br/>
+          <label for="location">Location:</label><br/>
+          <input type="text" name="location" v-model="logLocation"><br/><br/>
+          <label for="length">Length (Miles):</label><br/>
+          <input id="length-input" type="number" step="0.01" name="length" v-model="logLength"><br/><br/>
+          <label for="image">Image URL:</label><br/>
+          <input type="text" name="image" v-model="logImage"><br/><br/>
+          <label for="observations">Notes/Observations:</label><br/>
+          <textarea type="text" name="observations" v-model="logObservations"></textarea><br/><br/>
         <button type="submit">Create</button>
       </form>
     </div>
